44aa7992e0f568cc83580dd1f582e606e6001e5f
xeniabaturina <80832901+xeniabaturina@users.noreply.github.com>
2022-12-08 11:57:41 +0300

    Screen class updated

--- src/main/java/org/example/model/Screen.java
+++ src/main/java/org/example/model/Screen.java
@@ -2,11 +2,28 @@ package org.example.model;
 
 public class Screen {
 
-    int width;
-    int height;
+    private final int width;
+    private final int height;
+    private String title;
 
     public Screen(int width, int height) {
         this.width = width;
         this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
