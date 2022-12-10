7238bf38862ce5e2808dfa4385d951843961b53e
xeniabaturina <80832901+xeniabaturina@users.noreply.github.com>
2022-12-10 17:34:25 +0300

    Completed controller class, game is working

--- src/main/java/org/example/controller/FlappyBirdController.java
+++ src/main/java/org/example/controller/FlappyBirdController.java
@@ -1,15 +1,66 @@
 package org.example.controller;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import org.example.logic.Renderer;
+import org.example.logic.ChangeWorld;
+import org.example.model.Bird;
+import org.example.model.Columns;
+import org.example.model.Game;
+import org.example.model.Screen;
 
-public class FlappyBirdController implements ActionListener, MouseListener {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class FlappyBirdController implements ActionListener, MouseListener, KeyListener {
+
+    public Renderer renderer;
+    private Game game;
+    private Columns columns;
+    private final Bird bird;
+    private final Screen screen;
+    private final ChangeWorld changeWorld = new ChangeWorld(3);
+    Timer timer;
+    int WIDTH = 800;
+    int HEIGHT = 600;
+
+    public FlappyBirdController() {
+        this.game = new Game();
+        this.screen = new Screen(WIDTH, HEIGHT);
+        this.bird = new Bird(screen.getWidth() / 2 - 10, screen.getHeight() / 2 - 10, 20, 20);
+        this.columns = new Columns();
+        this.renderer = new Renderer(game, columns, bird, screen);
+        timer = new Timer(20, this);
+//        timer.start();
+    }
+
+    public Screen getScreen() {
+        return screen;
+    }
+
+    public void start() {
+        changeWorld.addColumns(screen, columns, 4);
+        timer.restart();
+    }
+
+    public Renderer getRenderer(){
+        return renderer;
+    }
+
+    public void restart() {
+        game = new Game();
+        columns = new Columns();
+        renderer.setGame(game);
+        renderer.setColumns(columns);
+        start();
+    }
 
     // region: ActionListener
     @Override
     public void actionPerformed(ActionEvent e) {
+        changeWorld.nextFrame(game, columns, bird, screen);
+        if (game.getGameStatus() == Game.GameStatus.GAME_OVER) {
+            restart();
+        }
+        renderer.repaint();
     }
     // end region
 
@@ -17,6 +68,11 @@ public class FlappyBirdController implements ActionListener, MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
+        if (game.getGameStatus() == Game.GameStatus.INITIAL) {
+            game.setGameStatus(Game.GameStatus.GAME_PLAYING);
+        }
+        changeWorld.jump(game, columns, bird, screen);
+        checkGameOverAndRepaint();
     }
 
     @Override
@@ -33,7 +89,28 @@ public class FlappyBirdController implements ActionListener, MouseListener {
 
     // end region
 
-    private void checkGameOver(){
-
+    private void checkGameOverAndRepaint(){
+        if (game.getGameStatus() == Game.GameStatus.GAME_OVER) {
+            restart();
+        }
+        renderer.repaint();
     }
+
+    // region: KeyListener
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if (game.getGameStatus() == Game.GameStatus.INITIAL) {
+            game.setGameStatus(Game.GameStatus.GAME_PLAYING);
+        }
+        changeWorld.jump(game, columns, bird, screen);
+        checkGameOverAndRepaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {/* ignore */}
+
+    @Override
+    public void keyReleased(KeyEvent e) {/* ignore */}
+
+    // end region
 }
--- src/main/java/org/example/logic/ChangeWorld.java
+++ src/main/java/org/example/logic/ChangeWorld.java
@@ -6,10 +6,12 @@ import org.example.model.Game;
 import org.example.model.Screen;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ChangeWorld {
     private int speed;
+    private final Random random = new Random();
 
     public ChangeWorld(int speed) {
         this.speed = speed;
@@ -21,7 +23,7 @@ public class ChangeWorld {
             Bird bird,
             Screen screen
     ) {
-        if (bird.getBird().y == 0 || bird.getBird().y == screen.getHeight()) {
+        if (bird.getBird().y == 0 || bird.getBird().y == screen.getHeight() - 140) {
             game.setGameStatus(Game.GameStatus.GAME_OVER);
         } else {
             for (Rectangle column : columns.getColumns()) {
@@ -36,6 +38,30 @@ public class ChangeWorld {
         }
     }
 
+    private void addColumn(Screen screen, Columns columns, int widthColumn, int space){
+        int heightColumn = 50 + random.nextInt(250);
+
+        int columnX;
+        if (columns.getSize() == 0) {
+            columnX = screen.getWidth() + widthColumn;
+        } else {
+            int firstColumnX = columns.getColumns().get(columns.getColumns().size() - 1).x;
+            columnX = firstColumnX + 600;
+        }
+        columns.add(columnX, screen.getHeight() - heightColumn - 120, widthColumn, heightColumn);
+        columns.add(columnX, 0, widthColumn, screen.getHeight() - heightColumn - space);
+
+    }
+
+    public void addColumns(Screen screen, Columns columns, int amount) {
+        int widthColumn = 100;
+        int space = 300;
+
+        for (int i = 0; i < amount; i++){
+            addColumn(screen, columns, widthColumn, space);
+        }
+    }
+
     // move columns on screen & move bird
     public void nextFrame(
             Game game,
@@ -43,31 +69,36 @@ public class ChangeWorld {
             Bird bird,
             Screen screen
     ) {
-        for (Rectangle column : columns.getColumns()) {
+        int passedColumns = 0;
+        ArrayList<Integer> columnsToRemove = new ArrayList<>();
+        for (int i = 0; i < columns.getSize(); i++) {
+            Rectangle column = columns.getColumns().get(i);
             int birdX = bird.getBird().x;
             int columnEdgeX = (column.x + column.width);
-            int columnCenterX = (columnEdgeX / 2);
+            int columnCenterX = columnEdgeX - (column.width / 2);
 
             column.x -= speed;
             if (column.y == 0) {
-                if (columnCenterX < birdX && columnCenterX + speed >= birdX) {
-                    game.passColumn();
+                if (columnCenterX < birdX && columnCenterX + speed > birdX) {
+                    System.out.println(birdX);
+                    System.out.println(i + "/" + columns.getSize() + " " + column.y + " " + column.x);
+                    passedColumns++;
                 }
                 if (columnEdgeX < 0) {
-                    Random random = new Random();
-                    int space = 300;
-                    int width = 100;
-                    int height = 50 + random.nextInt(300);
-
-                    int firstColumnX = columns.getColumns().get(columns.getColumns().size() - 1).x;
-                    columns.add(firstColumnX + 600, screen.getHeight() - height - 120, width, height);
-                    columns.add(firstColumnX, 0, width, screen.getHeight() - height - space);
-                    columns.remove();
-                    columns.remove();
+                    columnsToRemove.add(i);
                 }
             }
         }
-        bird.getBird().y += Math.min(speed / 2, screen.getHeight() - bird.getBird().y);
+
+        for (int i = 0; i < passedColumns; i++) {
+            game.passColumn();
+        }
+        for (int i = 0; i < columnsToRemove.size(); i++) {
+            columns.remove();
+            columns.remove();
+        }
+        addColumns(screen, columns, columnsToRemove.size());
+        bird.getBird().y += Math.min(speed / 2, (screen.getHeight() - 140) - bird.getBird().y);
         crashCheck(game, columns, bird, screen);
     }
 
@@ -78,7 +109,7 @@ public class ChangeWorld {
             Bird bird,
             Screen screen
     ) {
-        bird.getBird().y -= Math.min(10, bird.getBird().y);
+        bird.getBird().y -= Math.min(25, bird.getBird().y);
         crashCheck(game, columns, bird, screen);
     }
 
